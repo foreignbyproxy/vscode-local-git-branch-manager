1708453105595ca70deb82e297a47d2fa8f5c9a7
Gregory Gerard <dev@gregorygerard.me>
2023-01-12 23:41:39 -0500

    Adds basic functionality to retreive Git executatable

--- src/config.ts
+++ src/config.ts
@@ -12,4 +12,19 @@ export class Config {
 	get deletionStrategy() {
 		return this.config.get<DeleteStrategy>("delete-strategy");
 	}
+
+	get gitPaths() {
+		const configValue = vscode.workspace
+			.getConfiguration("git")
+			.get<string | string[] | null>("path", null);
+		if (configValue === null) {
+			return [];
+		} else if (typeof configValue === "string") {
+			return [configValue];
+		} else if (Array.isArray(configValue)) {
+			return configValue.filter((value) => typeof value === "string");
+		} else {
+			return [];
+		}
+	}
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,17 @@
 import * as vscode from "vscode";
+
+import { Config } from "./config";
 import { Logger } from "./logger";
+
+import { getGitExecutable } from "./utils";
 
 export async function activate(context: vscode.ExtensionContext) {
 	const logger = new Logger();
+
+	const config = new Config();
+
+	const gitExecutable = await getGitExecutable(context, config);
+
 	logger.log("Starting Git Branch Manager");
 	logger.log("Started Git Branch Manager");
 }
